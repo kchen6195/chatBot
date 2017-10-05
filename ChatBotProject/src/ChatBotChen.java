bfb6f9d7083d58685c69f12f238465a308452eac
Kelvin Chen <kchen6195@gmail.com>
2017-10-05 12:19:40 -0400

    Greetings for small animal

--- ChatBotProject/src/ChatBotChen.java
+++ ChatBotProject/src/ChatBotChen.java
@@ -1,4 +1,19 @@
-
+import java.util.Random;
+/*
+ * Chat Bot that will attend to a customer that wants a small animal.
+ * @Author: Kelvin Chen
+ * 
+ */
 public class ChatBotChen {
-//kelvin
+
+	int emotion = 0;
+	
+	/*
+	 * Get a greeting
+	 * @return: a greeting
+	 */
+	public String getGreeting()
+	{
+		return "Hello, I'm the bot responsible for small animals. Which include hamsters, guinea pigs and rabbits."; 
+	}
 }
