587517b3d55244fdd3a06017be921c4265bcdb83
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-10-05 13:06:16 -0400

    Edward

--- ChatBotProject/src/ChatBotYaroslavsky.java
+++ ChatBotProject/src/ChatBotYaroslavsky.java
@@ -1,4 +1,140 @@
+import java.util.Random;
 
+/**
+ * A program to carry on conversations with a human user about dogs and cats.
+ * This version:
+ * @author Edward Yaroslavsky
+ * @version October 2017
+ */
 public class ChatBotYaroslavsky { 
+	private boolean checkChatBot(String animalKey) {
+		String[] animalArray = {"dog","cat"};
+		for (String animal: animalArray) {
+/*			if (animal.equals(animalKey)) {
+				return true;
+			} */
+			if (findKeyword(animalKey,animal,0) != -1) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	//emotion can alter the way our bot responds. Emotion can become more negative or positive over time.
+	int emotion = 0;
+	/**
+	 * Get a default greeting 	
+	 * @return a greeting
+	 */	
+	public String getGreeting()
+	{
+		return "Hi, what is up?";
+	}
+		
+	/**
+	 * Gives a response to a user statement
+	 * 
+	 * @param statement
+	 *            the user statement
+	 * @return a response based on the rules given
+	 */
+	public String getResponse(String statement)
+	{
+		String response = "";
+		
+		if (statement.length() == 0)
+		{
+			response = "Say something, please.";
+		}
+		else if (findKeyword(statement, "no") >= 0)
+		{
+			response = "Why so negative?";
+                	emotion--;
+		}
+			
+		else if (findKeyword(statement, "levin") >= 0)
+		{
+			response = "More like LevinTheDream amiright?";
+			emotion++;
+		}
+		// Response transforming I want to statement
+		else if (findKeyword(statement, "I want to", 0) >= 0)
+		{
+			response = transformIWantToStatement(statement);
+		}
+		else if (findKeyword(statement, "I want",0) >= 0)
+		{
+			response = transformIWantStatement(statement);
+		}	
+		else
+		{
+			response = getRandomResponse();
+		}
+			
+		return response;
+	}
+	
+	/**
+	 * Search for one word in phrase. The search is not case
+	 * sensitive. This method will check that the given goal
+	 * is not a substring of a longer string (so, for
+	 * example, "I know" does not contain "no").
+	 *
+	 * @param statement
+	 *            the string to search
+	 * @param goal
+	 *            the string to search for
+	 * @param startPos
+	 *            the character of the string to begin the
+	 *            search at
+	 * @return the index of the first occurrence of goal in
+	 *         statement or -1 if it's not found
+	 */
+	private int findKeyword(String statement, String goal,
+			int startPos)
+	{
+		String phrase = statement.trim().toLowerCase();
+		goal = goal.toLowerCase();
 
+		// The only change to incorporate the startPos is in
+		// the line below
+		int psn = phrase.indexOf(goal, startPos);
+
+		// Refinement--make sure the goal isn't part of a
+		// word
+		while (psn >= 0)
+		{
+			// Find the string of length 1 before and after
+			// the word
+			String before = " ", after = " ";
+			if (psn > 0)
+			{
+				before = phrase.substring(psn - 1, psn);
+			}
+			if (psn + goal.length() < phrase.length())
+			{
+				after = phrase.substring(
+						psn + goal.length(),
+						psn + goal.length() + 1);
+			}
+
+			// If before and after aren't letters, we've
+			// found the word
+			if (((before.compareTo("a") < 0) || (before
+					.compareTo("z") > 0)) // before is not a
+											// letter
+					&& ((after.compareTo("a") < 0) || (after
+							.compareTo("z") > 0)))
+			{
+				return psn;
+			}
+
+			// The last position didn't work, so let's find
+			// the next, if there is one.
+			psn = phrase.indexOf(goal, psn + 1);
+
+		}
+
+		return -1;
+	}
 }
