cb923457dc3f70a96954699393c2492ae34a4e5f
Edward Yaroslavsky <eyaroslavsky8542@bths.edu>
2017-10-09 19:41:39 -0400

    added some methods to my bot but some arent finished

--- ChatBotProject/src/ChatBotYaroslavsky.java
+++ ChatBotProject/src/ChatBotYaroslavsky.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * A program to carry on conversations with a human user about dogs and cats.
@@ -13,7 +14,7 @@ public class ChatBotYaroslavsky {
 /*			if (animal.equals(animalKey)) {
 				return true;
 			} */
-			if (findKeyword(animalKey,animal,0) != -1) {
+			if (findKeyword(animalKey,animal) != -1) {
 				return true;
 			}
 		}
@@ -22,6 +23,10 @@ public class ChatBotYaroslavsky {
 	
 	//emotion can alter the way our bot responds. Emotion can become more negative or positive over time.
 	int emotion = 0;
+	String[] dogBreedArray = {"shepherd","husky","beagle","retriever","bulldog","chihuahua","poodle","boxer","rottweiler","dachshund",
+			"terrier","shih tzu","pug","corgi","dane","collie","spaniel","pomeranian","pointer","schnauzer","sheepdog","hound"};
+	boolean listedDogBreeds = false;
+	
 	/**
 	 * Get a default greeting 	
 	 * @return a greeting
@@ -33,11 +38,10 @@ public class ChatBotYaroslavsky {
 		
 	/**
 	 * Gives a response to a user statement
-	 * 
-	 * @param statement
-	 *            the user statement
+	 * @param statement the user statement
 	 * @return a response based on the rules given
 	 */
+	
 	public String getResponse(String statement)
 	{
 		String response = "";
@@ -49,63 +53,78 @@ public class ChatBotYaroslavsky {
 		else if (findKeyword(statement, "no") >= 0)
 		{
 			response = "Why so negative?";
-                	emotion--;
+            emotion--;
 		}
 			
-		else if (findKeyword(statement, "levin") >= 0)
+		else if (findKeyword(statement, "dog") >= 0)
 		{
-			response = "More like LevinTheDream amiright?";
+			response = "I love dogs!";
 			emotion++;
 		}
+		else if (findKeyword(statement, "cat") >= 0)
+		{
+			response = "Ew! Cats are disgusting creatures!";
+			emotion--;
+		}
 		// Response transforming I want to statement
-		else if (findKeyword(statement, "I want to", 0) >= 0)
+		else if (findKeyword(statement, "I want to") >= 0)
 		{
 			response = transformIWantToStatement(statement);
 		}
-		else if (findKeyword(statement, "I want",0) >= 0)
+		else if (findKeyword(statement, "I want") >= 0)
 		{
 			response = transformIWantStatement(statement);
 		}	
+		else if (findKeyword(statement, "I") >= 0 && findKeyword(statement, "you", findKeyword(statement, "I")) >= 0) {
+			response = transformIYouStatement(statement);
+		}
+		else if (findKeyword(statement, "decide") >= 0 && findKeyword(statement, "dog") >= 0) {
+			response = flipCoinGameIntro(statement);
+		}
+		if (statement.equals("play")) {
+			
+		}
+		else if (findKeyword(statement, "buy") >= 0) {
+			response = transformBuy(statement);
+		}
 		else
 		{
-			response = getRandomResponse();
+			for (String breed: dogBreedArray) {
+				if (findKeyword(statement,breed) != -1) {
+					listedDogBreeds = true;
+				}
+			}
+			if (listedDogBreeds) {
+				response = transformBreeds(statement);
+			}
+			else {
+				response = getRandomResponse();
+			}
 		}
 			
 		return response;
 	}
 	
 	/**
-	 * Search for one word in phrase. The search is not case
-	 * sensitive. This method will check that the given goal
-	 * is not a substring of a longer string (so, for
-	 * example, "I know" does not contain "no").
-	 *
-	 * @param statement
-	 *            the string to search
-	 * @param goal
-	 *            the string to search for
-	 * @param startPos
-	 *            the character of the string to begin the
-	 *            search at
-	 * @return the index of the first occurrence of goal in
-	 *         statement or -1 if it's not found
+	 * Search for one word in phrase. The search is not case sensitive. This method will check that the given goal 
+	 * is not a substring of a longer string (so, for example, "I know" does not contain "no").
+	 * @param statement the string to search
+	 * @param goal the string to search for
+	 * @param startPos the character of the string to begin the search at
+	 * @return the index of the first occurrence of goal in statement or -1 if it's not found
 	 */
-	private int findKeyword(String statement, String goal,
-			int startPos)
+	private int findKeyword(String statement, String goal, int startPos)
 	{
 		String phrase = statement.trim().toLowerCase();
 		goal = goal.toLowerCase();
 
-		// The only change to incorporate the startPos is in
-		// the line below
+		// The only change to incorporate the startPos is in the line below
 		int psn = phrase.indexOf(goal, startPos);
 
-		// Refinement--make sure the goal isn't part of a
-		// word
+		// Refinement--make sure the goal isn't part of a word
 		while (psn >= 0)
 		{
-			// Find the string of length 1 before and after
-			// the word
+			// Find the string of length 1 before and after the word
 			String before = " ", after = " ";
 			if (psn > 0)
 			{
@@ -113,28 +132,203 @@ public class ChatBotYaroslavsky {
 			}
 			if (psn + goal.length() < phrase.length())
 			{
-				after = phrase.substring(
-						psn + goal.length(),
-						psn + goal.length() + 1);
+				after = phrase.substring(psn + goal.length(), psn + goal.length() + 1);
 			}
 
-			// If before and after aren't letters, we've
-			// found the word
-			if (((before.compareTo("a") < 0) || (before
-					.compareTo("z") > 0)) // before is not a
-											// letter
-					&& ((after.compareTo("a") < 0) || (after
-							.compareTo("z") > 0)))
+			// If before and after aren't letters, we've found the word
+			if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0)) // before is not a letter
+					&& ((after.compareTo("a") < 0) || (after.compareTo("z") > 0)))
 			{
 				return psn;
 			}
 
-			// The last position didn't work, so let's find
-			// the next, if there is one.
+			// The last position didn't work, so let's find the next, if there is one.
 			psn = phrase.indexOf(goal, psn + 1);
 
 		}
 
 		return -1;
 	}
+	
+	/**
+	 * Search for one word in phrase.  The search is not case sensitive.
+	 * This method will check that the given goal is not a substring of a longer string
+	 * (so, for example, "I know" does not contain "no").  The search begins at the beginning of the string.  
+	 * @param statement the string to search
+	 * @param goal the string to search for
+	 * @return the index of the first occurrence of goal in statement or -1 if it's not found
+	 */
+	private int findKeyword(String statement, String goal)
+	{
+		return findKeyword (statement, goal, 0);
+	}
+	
+	/**
+	 * Take a statement with "I want to <something>." and transform it into 
+	 * "Why do you want to <something>?"
+	 * @param statement the user statement, assumed to contain "I want to"
+	 * @return the transformed statement
+	 */
+	private String transformIWantToStatement(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		int psn = findKeyword (statement, "I want to", 0);
+		String restOfStatement = statement.substring(psn + 9).trim();
+		return "Why do you want to " + restOfStatement + "?";
+	}
+	
+	/**
+	 * Take a statement with "I want <something>." and transform it into "Would you really be happy if you had <something>?"
+	 * @param statement the user statement, assumed to contain "I want"
+	 * @return the transformed statement
+	 */
+	private String transformIWantStatement(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		int psn = findKeyword (statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 6).trim();
+		return "Would you really be happy if you had " + restOfStatement + "?";
+	}
+	
+	/**
+	 * Take a statement with "I <something> you" and transform it into 
+	 * "Why do you <something> me?"
+	 * @param statement the user statement, assumed to contain "I" followed by "you"
+	 * @return the transformed statement
+	 */
+	private String transformIYouStatement(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		
+		int psnOfI = findKeyword (statement, "I", 0);
+		int psnOfYou = findKeyword (statement, "you", psnOfI);
+		
+		String restOfStatement = statement.substring(psnOfI + 1, psnOfYou).trim();
+		return "Why do you " + restOfStatement + " me?";
+	}
+	
+	/**
+	 * Take a statement with "buy" and figure out if the user wants dogs or cats and transforms into an agreement or disagreement
+	 * @param statement the user states, assumed to contain "buy", and possibly "dog" or "cat"
+	 * @return the transformed statement
+	 */
+	private String transformBuy(String statement) {
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		
+		int psn = findKeyword(statement, "buy");
+		String restOfStatement = statement.substring(psn + 3).trim();
+		
+		if (findKeyword(statement.substring(0, psn), "don't") >= 0 && findKeyword(statement, "cat") >= 0) {
+			emotion++;
+			return "Very good choice! You shouldn't even consider buying " + restOfStatement;
+		}
+		else if (findKeyword(statement.substring(0, psn), "don't") >= 0) {
+			return "Why don't you want to buy " + restOfStatement;
+		}
+		else if (findKeyword(statement, "cat") >= 0) {
+			emotion--;
+			return "Terrible Choice! You should not buy " + restOfStatement;
+		}
+		else if (findKeyword(statement, "dog") >= 0) {
+			emotion++;
+			return "I agree! Buying " + restOfStatement + " is a great idea!";
+		}
+		else if (findKeyword(statement.substring(0, psn), "don't") >= 0 && findKeyword(statement, "dog") >= 0) {
+			emotion--;
+			return "Let me try and persuade you into buying " + restOfStatement;
+		}
+		else {
+			return "Buying " + restOfStatement + " is boring.";
+		}
+	}
+	
+	
+	/**
+	 * Take a statement with a dog breed and respond back with a compliment to that breed
+	 * @param statement the user states, assumed to contain a dog breed
+	 * @return the transformed statement
+	 */
+	private String transformBreeds(String statement) {
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		
+		String dogBreeds = "";
+		
+		for (String breed: dogBreedArray) {
+			if (findKeyword(statement, breed) >= 0) {
+				dogBreeds += breed + ", ";
+			}
+		}
+		
+		emotion++;
+		return "I love " + dogBreeds + " they truly are fascinating!";
+	}
+	
+	private String flipCoinGameIntro(String statement) {
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		
+		int psn = findKeyword(statement, "decide");
+		String restOfStatement = statement.substring(psn + 6).trim();
+		
+		return "Let me help you decide " + restOfStatement + " by playing a game. \nType 'play' to begin";
+	}
+	
+	/**
+	 * Pick a default response to use if nothing else fits.
+	 * @return a non-committal string
+	 */
+	private String getRandomResponse ()
+	{
+		Random r = new Random ();
+		if (emotion == 0)
+		{	
+			return randomNeutralResponses [r.nextInt(randomNeutralResponses.length)];
+		}
+		if (emotion < 0)
+		{	
+			return randomAngryResponses [r.nextInt(randomAngryResponses.length)];
+		}	
+		return randomHappyResponses [r.nextInt(randomHappyResponses.length)];
+	}
+	
+	private String [] randomNeutralResponses = {"Interesting, tell me more", "Hmmm.", "Do you really think so?", "You don't say.", 
+			"It's all boolean to me.", "So, would you like to go for a walk?", "Could you say that again?"};
+	
+	private String [] randomAngryResponses = {"Bahumbug.", "Harumph", "The rage consumes me!"};
+	
+	private String [] randomHappyResponses = {"H A P P Y, what's that spell?", "Today is a good day", "You make me feel like a brand new pair of shoes."};
+	
+	
 }
